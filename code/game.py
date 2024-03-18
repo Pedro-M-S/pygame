2f3003425875936b05606f06dc31adbe39acc5b7
pedrosampaio <1231853@isep.ipp.pt>
2024-03-18 19:33:36 +0000

    updated game

--- code/game.py
+++ code/game.py
@@ -1,20 +1,33 @@
 import pygame, sys
 from settings import *
 
+from level import Level
+
 class Game:
     def __init__(self):
         pygame.init()
-        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        self.screen = pygame.display.set_mode((WIDTH * 3, HEIGHT * 3))
+        pygame.display.set_caption('pygame_lolo')
         self.clock = pygame.time.Clock()
 
+        self.level = Level()
+
+        
     def run(self):
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        pygame.quit()
+                        sys.exit()
             
-            self.screen.fill((0, 0, 0))
+            
+            self.level.run()
+            self.screen.blit(pygame.transform.scale(self.level.display_surface,(WIDTH * 3, HEIGHT * 3)), (0, 0))
+            
             pygame.display.update()
             self.clock.tick(FPS)
         
